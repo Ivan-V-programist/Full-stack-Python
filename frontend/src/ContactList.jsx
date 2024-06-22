0e5c022870343b6382511a0de53671720375bf46
Ivan-V-programist <ivanvinokurov965@gmail.com>
2024-06-22 22:32:56 +0300

    established connection between front and backend

---
+++ frontend/src/ContactList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ContactList = ({contacts}) => {
+    return <div>
+        <h2>Contacts</h2>
+        <table>
+            <thead></thead>
+            <tbody></tbody>
+        </table>
+    </div>
+
+}
